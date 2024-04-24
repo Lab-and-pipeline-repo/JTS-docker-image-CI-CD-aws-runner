74a25b68b28a6f2cdcccd21914f3ccaf343ed453
subash2devopsintern <subaschy729@gmail.com>
2024-04-24 21:37:23 +0545

    old-database-config

--- scripts/spawn_container.sh
+++ scripts/spawn_container.sh
@@ -17,7 +17,7 @@ sudo docker network create --subnet=172.30.0.0/24 bridge-subas-script-172.30.0.0
 echo "==creating database container===="
 
 sudo docker run  -itd --name tododb-container -e POSTGRES_USER=subash  -e POSTGRES_PASSWORD=subash@123 -e POSTGRES_DB=tododb --network=bridge-subas-script-172.30.0.0-sl-24 --ip 172.30.0.2 -p 5432:5432 subash729/todo-database:stable
-sleep 15
+sleep 50
 # Start container Backend
 echo "==creating backend container===="
 sudo docker run -itd --name todoback-container --network=bridge-subas-script-172.30.0.0-sl-24 --ip 172.30.0.3  --env-file /home/ec2-user/secret/.env  -p 3000:3000 subash729/todo-backend:stable
