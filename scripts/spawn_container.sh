386055e9a314c62632eb0a233e2755508be45f98
subash2devopsintern <subaschy729@gmail.com>
2024-04-24 11:14:39 +0545

    testing deploy script

--- scripts/spawn_container.sh
+++ scripts/spawn_container.sh
@@ -14,19 +14,18 @@ echo " "
 echo "Creating network bridge"
 sudo docker network create --subnet=172.30.0.0/24 bridge-subas-script-172.30.0.0-sl-24 --driver bridge
 
-
 # Start container database
 echo "==creating database container===="
 
 sudo docker run  -itd --name tododb-container -e POSTGRES_USER=subash  -e POSTGRES_PASSWORD=subash@123 -e POSTGRES_DB=tododb --network=bridge-subas-script-172.30.0.0-sl-24 --ip 172.30.0.2 -p 5432:5432 subash729/todo-database:stable 
-
+sleep 15
 # Start container Backend
 echo "==creating backend container===="
-sudo docker run -itd --name todoback-container --network=bridge-subas-script-172.30.0.0-sl-24 --ip 172.30.0.3  -p 3000:3000 subash729/todo-backend:stable
+sudo docker run -itd --name todoback-container --network=bridge-subas-script-172.30.0.0-sl-24 --ip 172.30.0.3  --env-file ~/secret/.env -p 3000:3000 subash729/todo-backend:stable
 
 # Start container front
 echo "==creating frontend container===="
-sudo docker run -itd --name todofront-container --network=bridge-subas-script-172.30.0.0-sl-24 --ip 172.30.0.4  -p 5173:5173 subash729/todo-frontend:stable 
+sudo docker run -itd --name todofront-container --network=bridge-subas-script-172.30.0.0-sl-24 --ip 172.30.0.4  -p 5173:5173 subash729/todo-frontend:stable
 
 echo " "
 echo " "
